b4fdbeb9a4b570be3abda54f51728b88ea693b90
Ionflux <webmaster@ionflux.org>
2013-06-22 11:35:09 +0000

    some work on the grid generator

--- geoutils/test/blender/bgridtest01.py
+++ geoutils/test/blender/bgridtest01.py
@@ -5,17 +5,22 @@ import BGeoUtils as bg
 import BGeoUtils.Object as bgo
 import BGeoUtils.Material as bm
 
-def createGrid(gridName, direction, mat, gridStart, gridStep1, gridStep2, 
-    gridStop, axis1, axis2, emphasizeAxes = True):
+def createGrid(gridName, direction, mat1, mat2, gridStart, gridStep1, gridStep2, 
+    gridStop, axis1, axis2, emphasizeAxes = True, emphasizePlane = True):
     cp = cg.Vector3(gridStart)
     i = 0
     j = 0
     while (not im.gt(cp[axis2], gridStop[axis2])):
         while (not im.gt(cp[axis1], gridStop[axis1])):
             width = 0.001
-            if (im.eq(cp[axis1], 0.) and im.eq(cp[axis2], 0) 
+            mat = mat1
+            if (im.eq(cp[axis2], 0., 0.1) 
+                and emphasizePlane):
+                width = width * 80
+                mat = mat2
+            if (im.eq(cp[axis1], 0., 0.1) and im.eq(cp[axis2], 0., 0.1) 
                 and emphasizeAxes):
-                width = 0.004
+                width = width * 4
             cyl = bgo.Cylinder("Cyl_" + gridName + ("_%03d_%03d" % (i, j)), 
                 10, 10., width)
             cyl.createBObject()
@@ -34,6 +39,9 @@ mm.addDefaultMaterials()
 
 gridStep = 1.
 
+blueMat = mm.getMaterial('Blue')
+redMat = mm.getMaterial('Red')
+greenMat = mm.getMaterial('Green')
 glowingBlueMat = mm.getMaterial('GlowingBlue')
 glowingRedMat = mm.getMaterial('GlowingRed')
 glowingGreenMat = mm.getMaterial('GlowingGreen')
@@ -47,8 +55,8 @@ gridStep20 = cg.Vector3(0., gridStep * 2., 0.)
 gridStop0 = cg.Vector3(5., 5., 0.)
 direction0 = cg.Vector3.E_Z
 
-createGrid("XY", direction0, glowingRedMat, gridStart0, gridStep10, 
-    gridStep20, gridStop0, axis10, axis20)
+createGrid("XY", direction0, redMat, glowingRedMat, gridStart0, gridStep10, 
+    gridStep20, gridStop0, axis10, axis20, True, True)
 
 axis11 = cg.AXIS_X
 axis21 = cg.AXIS_Z
@@ -59,8 +67,8 @@ gridStep21 = cg.Vector3(0., 0., gridStep)
 gridStop1 = cg.Vector3(5., 0., 5.)
 direction1 = cg.Vector3.E_Y
 
-createGrid("XZ", direction1, glowingBlueMat, gridStart1, gridStep11, 
-    gridStep21, gridStop1, axis11, axis21)
+createGrid("XZ", direction1, blueMat, glowingBlueMat, gridStart1, gridStep11, 
+    gridStep21, gridStop1, axis11, axis21, True, False)
 
 axis12 = cg.AXIS_Y
 axis22 = cg.AXIS_Z
@@ -71,6 +79,6 @@ gridStep22 = cg.Vector3(0., 0., gridStep)
 gridStop2 = cg.Vector3(0., 5., 5.)
 direction2 = cg.Vector3.E_X
 
-createGrid("YZ", direction2, glowingGreenMat, gridStart2, gridStep12, 
-    gridStep22, gridStop2, axis12, axis22)
+createGrid("YZ", direction2, greenMat, glowingGreenMat, gridStart2, gridStep12, 
+    gridStep22, gridStop2, axis12, axis22, True, False)
 
